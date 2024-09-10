d9dd7ca8567f686222f0ff921279bc50c9ed7dbd
Garry <6753180+grrywlsn@users.noreply.github.com>
2024-09-10 22:32:45 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+softwareupdate --all --install --force
+sudo xcode-select --install
+
+### Install/upgrade Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
 cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
