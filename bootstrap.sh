0f866257dfa73e4e3f8ac30a6fbdb7b79a39f876
Garry <6753180+grrywlsn@users.noreply.github.com>
2024-09-10 22:57:31 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,28 +6,4 @@ sudo xcode-select --install
 ### Install/upgrade Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-cd "$(dirname "${BASH_SOURCE}")";
-
-git pull origin main;
-
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
-}
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+source ~/.bash_profile;
